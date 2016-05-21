5c7befb66ae1a2f86377b4cb08efac12c3bf0495
victor-suriel <victor.v.suriel@gmail.com>
2016-05-21 13:02:23 -0400

    Add files via upload

---
+++ jacPlot.m
@@ -0,0 +1,18 @@
+fileID = fopen('/Users/EscobarWest/data.txt')
+A = fscanf(fileID, '%f');
+
+N = A(1);
+
+T = A(2);
+
+h = A(3);
+
+A = vec2mat(A(4:end), N);
+
+[X, Y] = meshgrid( linspace(-T, T, N), linspace(-T, T, N) );
+
+figure
+surf(X, Y, A)
+title('JackMPI')
+
+fclose(fileID)
